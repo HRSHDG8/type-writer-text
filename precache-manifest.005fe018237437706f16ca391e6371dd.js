0f9526f8b576e27b7a117526744ee5c4dcf796b3
hrshdg8 <hrshdg8@gmail.com>
2020-04-11 19:23:23 +0530

    Updates

---
+++ precache-manifest.005fe018237437706f16ca391e6371dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3804b40af1d24f5b3505c9e4adca33e5",
+    "url": "/type-writer-text/index.html"
+  },
+  {
+    "revision": "d0ae66ce2ea080a931e4",
+    "url": "/type-writer-text/static/css/main.d202d9ae.chunk.css"
+  },
+  {
+    "revision": "2af88f0b4417e50b98c4",
+    "url": "/type-writer-text/static/js/2.ab98f484.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/type-writer-text/static/js/2.ab98f484.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0ae66ce2ea080a931e4",
+    "url": "/type-writer-text/static/js/main.061c4da3.chunk.js"
+  },
+  {
+    "revision": "ddb33c9412e09f4443e6",
+    "url": "/type-writer-text/static/js/runtime-main.9b17a465.js"
+  }
+]);
